2d0ea99a1adea24c621829dcc287db79d55210a3
Kajol sutra dhar <kajol.sd777@gmail.com>
2024-03-25 14:26:44 +0200

    Event handler

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,13 @@ import PersonList from "./assets/components/PersonsList";
 function App() {
   const [counter, setCounter] = useState(0);
 
+  const clickHandler = ()=>{
+    console.log(`Handler on duty`);
+  }
   return (
     <>
       <p>Counter: {counter} </p>
+      <button onClick={clickHandler}>Count</button>
       <PersonList/>
 
     </>
